c2292db86a9d1eeccdc9fd79c107dab732f5362c
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2022-10-28 19:22:46 +0530

    Add files via upload

---
+++ views/index.tsx
@@ -0,0 +1,6 @@
+export * from "./search/SearchPage";
+export * from "./register/RegisterPage";
+export * from "./login/LoginPage";
+export * from "./history/HistoryPage";
+export * from "./itemsplaylist/ItemsPlaylist";
+export * from "./otheruser/OtherUserPage";
